643e3ca68104c86d6fdd7e00aed1bf6f449a0032
Juergen Schaar <j.schaar@blockchainfirst.org>
2018-04-06 15:51:45 +0200

    include language support i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+
+type Language = 'en' | 'de' | 'fr' | 'th';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  translate: TranslateService;
+  constructor(translate: TranslateService){
+    this.translate = translate;    
+    translate.addLangs(['en', 'de', 'fr', 'th']);
+    translate.setDefaultLang('en');  
+  }
+
+  switchLanguage = (lang: Language) => {
+    this.translate.use(lang);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -17,7 +21,17 @@ import { AboutComponent } from './components/about/about.component';
   imports: [
     BrowserModule,
     MDBBootstrapModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: function createTranslateLoader(http: HttpClient){
+          return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+        },
+        deps: [HttpClient]
+      }
+    })
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
