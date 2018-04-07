5c37716c551c94793ce0a3e964e1456ec737a8e4
Juergen Schaar <j.schaar@blockchainfirst.org>
2018-04-07 20:59:04 +0200

    language select dropdown added in main menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 
-type Language = 'en' | 'de' | 'fr' | 'th';
+//type Language = 'en' | 'de' | 'fr' | 'th';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +9,46 @@ type Language = 'en' | 'de' | 'fr' | 'th';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app';  
+  languages: Array<any> = [{
+      name: 'English',
+      short: 'en'
+    },{
+      name: 'Deutsch',
+      short: 'de'
+    },{
+      name: 'Thai',
+      short: 'th'
+    },{
+      name: 'France',
+      short: 'fr'
+    }
+  ]
+  language:any;
   translate: TranslateService;
   constructor(translate: TranslateService){
     this.translate = translate;    
-    translate.addLangs(['en', 'de', 'fr', 'th']);
-    translate.setDefaultLang('en');  
+    this.languages.forEach((language) => {
+       //console.log('language: ', language)       
+       translate.addLangs([language.short])
+    });    
+    translate.setDefaultLang(translate.getLangs()[0]);  
+    this.language = this.languages[0];
+    //console.log('getLangs: ', translate.getLangs())
   }
 
-  switchLanguage = (lang: Language) => {
-    this.translate.use(lang);
-  }
+  switchLanguage(language) {
+    console.log(language);
+    this.translate.use(language.short);
+    this.language = language;
+  }    
+  
+  public isOpenChange(event): void { console.log('isOpenChange triggered:' , event); }
+
+  public onHidden(event): void { console.log('onHidden triggered!' , event); }
+
+  public onShown(event): void { console.log('OnShown triggered!' , event); }
+
+  public change(event): void { console.log('change triggered: ', event); }
+  
 }
